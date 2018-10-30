8fc0c185c270fb4ed30ecec9a8c13f482839d74a
jtbamford <tom.bamford5@yahoo.co.uk>
2018-10-30 14:28:05 +0000

    branch commit

--- AccountProject/src/main/java/com/qa/AccountProject/Runner.java
+++ AccountProject/src/main/java/com/qa/AccountProject/Runner.java
@@ -2,9 +2,18 @@ package com.qa.AccountProject;
 
 import java.util.HashMap;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+
 public class Runner {
 	
 	public static void main(String args[]) {
+		
+		ObjectMapper mapper = new ObjectMapper();
 		
 		HashMap<Integer, Account> accountList = new HashMap<Integer, Account>();
 		
@@ -17,6 +26,22 @@ public class Runner {
 		System.out.println(Service.retrieveAccount(acc1,accountList));
 		
 		
+		//String ans=mapper.writeValueAsString(acc1);
+		
+		try {
+
+			// Convert object to JSON string
+			String jsonInString = mapper.writeValueAsString(acc1);
+			System.out.println(jsonInString);
+
+
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		
 	}
